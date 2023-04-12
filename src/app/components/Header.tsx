5b1d1f94c643f57109426b46b4d3ea257df3506d
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-12 09:01:15 +0900

    app: top page

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <div className="flex items-baseline text-xl gap-10 px-20 py-6">
+      <h1 className="text-2xl">Logo</h1>
+    </div>
+  );
+}

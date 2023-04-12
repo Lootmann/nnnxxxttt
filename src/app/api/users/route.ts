712165089d69d616d0394a656ea76298cc191958
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-12 13:18:46 +0900

    api/users: get query strings

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/prisma";
+import { NextApiRequest } from "next";
 
-export async function GET(req: NextRequest) {
-  const users = await prisma.user.findMany();
+export async function GET(req: NextApiRequest) {
+  if (req.url === undefined) {
+    const users = await prisma.user.findMany();
+    return NextResponse.json({ users });
+  }
 
-  return NextResponse.json({ users }, { status: 200 });
+  // NOTE: get query string
+  const { searchParams } = new URL(req.url);
+  const name = searchParams.get("name");
+
+  if (name === null) {
+    const users = await prisma.user.findMany();
+    return NextResponse.json({ users });
+  }
+
+  const users = await prisma.user.findMany({
+    where: {
+      name: name,
+    },
+  });
+
+  return NextResponse.json({ users });
 }

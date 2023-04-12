4759526fb3b0436d10464cf953632bcbce2aa13d
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-12 15:16:32 +0900

    app/posts: get all posts, get a post by Id

---
+++ src/app/posts/[postId]/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type PageProps = {
+  params: {
+    postId: number;
+  };
+};
+
+async function getPostById(postId: number) {
+  const res = await fetch(`http://localhost:3000/api/posts/${postId}`);
+  if (!res.ok) throw new Error("posts/[postId]/page.tsx");
+
+  const data = await res.json();
+  return data.post as TPost;
+}
+
+export default async function Page({ params }: PageProps) {
+  const post = await getPostById(params.postId);
+
+  return (
+    <div className="flex flex-col gap-4 text-xl">
+      <h2 className="text-2xl">/posts/{params.postId}/pages.tsx</h2>
+
+      <div className="border p-4">
+        <p>ID: {post.id}</p>
+        <p>Title: {post.title}</p>
+        <p>Content: {post.title}</p>
+        <p>
+          Author: {post.author.id} {post.author.name}
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -11,18 +11,21 @@ async function getPosts() {
 
 export default async function Page() {
   const posts = await getPosts();
+
   return (
     <div className="flex flex-col gap-4 text-xl">
       <h2 className="text-2xl">/hoge/posts.tsx</h2>
 
-      <div>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>
-              {post.id}, {post.title}
-            </li>
-          ))}
-        </ul>
+      <div className="flex flex-col gap-4">
+        {posts.map((post) => (
+          <Link
+            href={`/posts/${post.id}`}
+            key={post.id}
+            className="hover:bg-slate-700 px-2 rounded-md"
+          >
+            {post.id}, {post.title}
+          </Link>
+        ))}
       </div>
     </div>
   );

dbda105455da58d01773df65b152dd4c6b528c90
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-14 07:46:04 +0900

    api/auths/login: login

--- src/app/api/auths/login/route.ts
+++ src/app/api/auths/login/route.ts
@@ -5,7 +5,32 @@ import { prisma } from "@/prisma/prisma";
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const { name } = body;
-  console.log(name);
 
-  return NextResponse.json({ msg: "success to login" });
+  const user = await prisma.user.findUnique({
+    where: {
+      name: name,
+    },
+  });
+
+  if (user) {
+    return NextResponse.json(
+      { msg: "success to login" },
+      {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    );
+  } else {
+    return NextResponse.json(
+      { msg: "fail to login" },
+      {
+        status: 401,
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    );
+  }
 }
